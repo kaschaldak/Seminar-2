b736a73a93f63e981df3cf1ad25650d434145da6
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-23 20:06:56 +0500

    finding the second number in a three digit number

---
+++ a task 1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Enterb number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 99 && num <1000)
+{
+    int secondNumber = (num / 10) % 10; //895/10 = 85%10=9
+    Console.WriteLine($"вторая цифра в числе {num} = > {secondNumber}");
+}
+else
+Console.WriteLine($"Число {num} не трехзначное");
