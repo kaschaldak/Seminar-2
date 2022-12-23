6ba4d6197a049b2fe9ad32f8a78ee476ca5a2a4a
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-23 20:34:29 +0500

    search index number

---
+++ a task 2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Enterb number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введте число: ");
+int index = Convert.ToInt32(Console.ReadLine());
+
+double pow = Math.Pow(10, index);
+Console.Clear();
+
+if(num < pow)
+{
+    Console.WriteLine($"В числе {num} нет третьей цифры" );
+   
+}
+
+else
+{
+    int tmp = num;
+
+while (tmp > pow )
+{
+    tmp/=10;
+}
+int result = tmp % 10;
+Console.WriteLine($"Цифра под индексом {index} числа {num} ->{result} ");
+}
